e7c74ebdf3451c828afcc6383dd28f2385760d91
aslemammad <zorofight94@gmail.com>
2021-09-25 22:20:42 +0330

    fix redis

--- src/utils/asyncRedis.ts
+++ src/utils/asyncRedis.ts
@@ -1,11 +1,16 @@
 import * as redis from "redis";
 import { promisify } from "util";
 
-const client = redis.createClient({
-  host: process.env.REDIS_HOST,
-  password: process.env.REDIS_PASS,
-  port: Number(process.env.REDIS_PORT),
-});
+// const client = redis.createClient({
+//   host: process.env.REDIS_HOST,
+//   password: process.env.REDIS_PASS,
+//   port: Number(process.env.REDIS_PORT),
+// });
+const client = redis.createClient(
+  Number(process.env.REDIS_PORT),
+  process.env.REDIS_HOST
+);
+client.auth(process.env.REDIS_PASS);
 
 export const get: (key: string) => Promise<string> = promisify(client.get).bind(
   client
