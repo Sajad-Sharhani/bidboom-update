f7c4d077e1cef614cf9a3c47f15ea98fc4f5b340
aslemammad <zorofight94@gmail.com>
2021-09-26 02:06:12 +0330

    update

--- src/utils/asyncRedis.ts
+++ src/utils/asyncRedis.ts
@@ -1,36 +1,22 @@
 import * as redis from "redis";
 import { promisify } from "util";
 
-// const client = redis.createClient({
-//   host: process.env.REDIS_HOST,
-//   password: process.env.REDIS_PASS,
-//   port: Number(process.env.REDIS_PORT),
-// });
-let cache: redis.RedisClient;
+const client = redis.createClient({
+  host: process.env.REDIS_HOST,
+  password: process.env.REDIS_PASS,
+  port: Number(process.env.REDIS_PORT),
+});
 
-const getClient = () => {
-  if (cache) return cache;
+export const get: (key: string) => Promise<string> = promisify(client.get).bind(
+  client
+);
 
-  const client = redis.createClient(
-    Number(process.env.REDIS_PORT),
-    process.env.REDIS_HOST
-  );
-  client.auth(process.env.REDIS_PASS);
-
-  return (cache = client);
-};
-
-export const get: (key: string) => Promise<string> = (key) =>
-  promisify(getClient().get).bind(getClient())(key);
-
-export const set: (key: string, value: string) => Promise<string> = (
-  key,
-  value
-) => promisify(getClient().set).bind(getClient())(key, value);
+export const set: (key: string, value: string) => Promise<string> = promisify(
+  client.set
+).bind(client);
 
 export const setex: (
   key: string,
   seconds: number,
   value: string
-) => Promise<string> = (key, seconds, value) =>
-  promisify(getClient().setex).bind(getClient())(key, seconds, value);
+) => Promise<string> = promisify(client.setex).bind(client);
